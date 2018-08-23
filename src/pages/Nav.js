58a637a6eee192e44eb0de766da39063abe254d5
Meg Scholl <meghan.e.scholl@gmail.com>
2018-08-23 15:14:17 -0500

    deleted Main.js and Main.css and renameda Nav.js and a Map.js

---
+++ src/pages/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Nav.css'
+import logo from '../images/weedwrangle/ww_logo.png';
+
+class Nav extends Component {
+    render() {
+        return (
+            <div>            
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            </div>
+        );
+    }
+}
+
+export default Nav;
