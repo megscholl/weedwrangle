75a6de32c225c6446ad6ad155d6b5d329d316c40
Meg Scholl <meghan.e.scholl@gmail.com>
2018-08-23 15:14:36 -0500

    Working on App.css to bring in routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './images/weedwrangle/ww_logo.png';
 import './App.css';
-import Main from './pages/Main.js'
+import Nav from './pages/Nav.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
-      // <Main />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Nav />
+
       </div>
     );
   }
