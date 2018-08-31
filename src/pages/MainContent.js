e28b7b99e6f6e5382a504951ea4f907d0faeeb66
Meg Scholl <meghan.e.scholl@gmail.com>
2018-08-30 23:34:15 -0500

    no changes...was messing with text-aligns and colors

--- src/pages/MainContent.js
+++ src/pages/MainContent.js
@@ -6,13 +6,13 @@ class MainContent extends Component {
         return (
             <div className="main-p-content">
 
-                <p>Weed Wrangle&reg;, is a one-day, citywide, volunteer effort to help rescue our public parks and green spaces from invasive species through hands-on removal of especially harmful trees, vines and flowering plants. Typical unwelcome plants are honeysuckle (Lonicera japonica and L. maackii), Chinese privet (Ligustrum sinense), winter creeper (Euonymus fortunei), autumn olive (Elaeagnus umbellata var. parviflora), English ivy (Hedera helix) and kudzu (Pueraria montana var. lobata).
-
-                </p>
-
+                <p>Weed Wrangle&reg;, is a one-day, citywide, volunteer effort to help rescue our public parks and green spaces from invasive species through hands-on removal of especially harmful trees, vines and flowering plants. Typical unwelcome plants are honeysuckle (Lonicera japonica and L. maackii), Chinese privet (Ligustrum sinense), winter creeper (Euonymus fortunei), autumn olive (Elaeagnus umbellata var. parviflora), English ivy (Hedera helix) and kudzu (Pueraria montana var. lobata).</p>
+<br />
                 <p>
                 Supervised by an experts in invasive weed management, Weed Wrangle&reg;-volunteers will learn, practice, and begin a habit of maintaining an area free of invasive plants and encourage replanting with natives in removal areas. By engaging our neighbors and challenging them to take action in their own spaces, we hope to create a movement that will have the greatest impact on the invasive plant population.
                 </p>
+
+
             </div>
         )
     }
