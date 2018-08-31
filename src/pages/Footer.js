e4798922557b011a5c58221ce49731969b0a1865
Meg Scholl <meghan.e.scholl@gmail.com>
2018-08-31 10:16:30 -0500

    added alt tag to footer GCA image

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -10,7 +10,7 @@ class Footer extends Component {
 
 
                 <p className="footer-p1">The Weed Wrangle&reg; started by The Garden Club of Nashville in 2015, expanded to the Knoxville Garden Club, Garden Club of Lookout Mountain, Memphis Garden Club, The Little Garden Club of Memphis, and The Garden Club of Jackson, MS in 2016. All clubs are members of The Garden Club of America and participants in the GCA Partners for Plants program.
-                <img src={GCA} className="gca-image"/>
+                <img src={GCA} alt="Garden Club of America" className="gca-image"/>
                 </p>
             </footer>
         </div>
