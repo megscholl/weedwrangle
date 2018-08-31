a5633d0c86ee5b6571f2d49add7bfbe49ed581d2
Meg Scholl <meghan.e.scholl@gmail.com>
2018-08-31 10:35:21 -0500

    added h1 and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Nav from './Nav.js'
 import Video from './Video.js'
 import Map from './Map.js'
 import MainContent from './MainContent.js'
@@ -9,7 +8,7 @@ class Home extends Component {
       return (
         <div>
   
-         <Nav />
+         <h1>Weed Wrangle is a Volunteer-Led Invasive Plant Removal Initiative</h1>
          <Video />
          <MainContent />
          <Map />
