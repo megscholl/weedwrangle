dd39ba9c68728b1fd189cc4759e9f50dfa533dc6
Meg Scholl <meghan.e.scholl@gmail.com>
2018-08-31 11:23:18 -0500

    added more border-radius main images

--- src/pages/MainContent.js
+++ src/pages/MainContent.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import './HomeContent.css'
+import honeysuckle from '../images/weedwrangle/honeysuckle.jpg'
+import loosestrife from '../images/weedwrangle/purple-loosestrife.jpg'
+import ivy from '../images/weedwrangle/english-ivy.jpg'
 
 class MainContent extends Component {
     render() {
@@ -7,11 +10,18 @@ class MainContent extends Component {
             <div className="main-p-content">
 
                 <p>Weed Wrangle&reg;, is a one-day, citywide, volunteer effort to help rescue our public parks and green spaces from invasive species through hands-on removal of especially harmful trees, vines and flowering plants. Typical unwelcome plants are honeysuckle (Lonicera japonica and L. maackii), Chinese privet (Ligustrum sinense), winter creeper (Euonymus fortunei), autumn olive (Elaeagnus umbellata var. parviflora), English ivy (Hedera helix) and kudzu (Pueraria montana var. lobata).</p>
-<br />
+                
+                <br />
+                
                 <p>
                 Supervised by an experts in invasive weed management, Weed Wrangle&reg;-volunteers will learn, practice, and begin a habit of maintaining an area free of invasive plants and encourage replanting with natives in removal areas. By engaging our neighbors and challenging them to take action in their own spaces, we hope to create a movement that will have the greatest impact on the invasive plant population.
                 </p>
 
+                <div className="main-images">
+                <img src={honeysuckle} alt="honeysuckle" className="radius-imgs"/>
+                <img src={loosestrife} alt="purple loosestrife" className="radius-imgs"/>
+                <img src={ivy} alt="english ivy" className="radius-imgs"/>
+                </div>
 
             </div>
         )
