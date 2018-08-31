b006d6c276b08e5bbe0d2d0174781a18b43b8410
Meg Scholl <meghan.e.scholl@gmail.com>
2018-08-30 23:28:44 -0500

    added footer with some details..more to come

---
+++ src/pages/Footer.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import GCA from '../images/weedwrangle/GCA-Logo.png'
+import './Footer.css'
+
+class Footer extends Component {
+    render() {
+      return (
+        <div>
+            <footer className="footer">
+
+
+                <p className="footer-p1">The Weed Wrangle&reg; started by The Garden Club of Nashville in 2015, expanded to the Knoxville Garden Club, Garden Club of Lookout Mountain, Memphis Garden Club, The Little Garden Club of Memphis, and The Garden Club of Jackson, MS in 2016. All clubs are members of The Garden Club of America and participants in the GCA Partners for Plants program.
+                <img src={GCA} className="gca-image"/>
+                </p>
+            </footer>
+        </div>
+     )
+    }
+}
+
+export default Footer;
