3203d8fbf081ad5c094684e6b67659268e1a9e60
Meg Scholl <meghan.e.scholl@gmail.com>
2018-08-30 22:47:05 -0500

    all other small detailed changes including added new css for maincontent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Nav from './pages/Nav.js'
-import Video from './pages/Video.js'
-import Map from './pages/Map.js'
+import Home from './pages/Home.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
-       <Nav />
-       <Video />
-       <Map />
+       <Home />
 
       </div>
     );
