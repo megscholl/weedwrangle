148e51e3c3b068d1238183e80c6312919fa291fe
Meg Scholl <meghan.e.scholl@gmail.com>
2018-09-20 15:51:09 -0400

    changes to many files, tried getting the document downloads to work but theyre more difficult...also, error page changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Main from './pages/Main.js'
 import About from './pages/navigation/About.js'
 import Press from './pages/navigation/Press.js'
 import Contact from './pages/navigation/Contact.js'
+
+// STATES FROM MAP
+import Tennessee from'./pages/states/Tennessee'
+import Louisiana from'./pages/states/Louisiana'
+import Texas from'./pages/states/Texas'
+import Oregon from'./pages/states/Oregon'
+import Virginia from'./pages/states/Virginia'
+import Mississippi from'./pages/states/Mississippi'
+
+
 import Error from './pages/Error.js'
 import Navigation from './pages/Nav.js'
 
@@ -20,6 +30,12 @@ class App extends Component {
             <Route path="/about" component={About} />
             <Route path="/press" component={Press} />
             <Route path="/contact" component={Contact} />
+            <Route path="/tn" component={Tennessee} />
+            <Route path="/la" component={Louisiana} />
+            <Route path="/tx" component={Texas} />
+            <Route path="/or" component={Oregon} />
+            <Route path="/va" component={Virginia} />
+            <Route path="/ms" component={Mississippi} />
             <Route component={Error}/>
           </Switch>
         </div>
