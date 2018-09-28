7cf65376ee33987938c8415ee307c77a0be8df8a
Meg Scholl <meghan.e.scholl@gmail.com>
2018-09-28 13:44:49 -0500

    resources content and extra details

--- src/pages/navigation/Resources.js
+++ src/pages/navigation/Resources.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Resources.css'
 
+import WW from './IMG_129.jpg'
+
 const Resources = () =>{
   return ( 
     <div>
 
         <h1>Educational Resources & FAQ</h1>
+
+        <img src={WW} alt="Images" width="40%" className="resource-img"/>
         
         <div  className="FAQ-page">
             <ul>
@@ -29,8 +33,8 @@ const Resources = () =>{
                         <li>Texas</li>
                         <li>Virginia</li>
                     </ul>
-                <li className="ul-li">What are some common invasive plants?</li>
-                <li className="ul-li">What are some common invasive plants?</li>
+                <li className="ul-li">What are best practices for getting rid of invasive plants?</li>
+                    <p className="li-p">The best way to get rid of invasive plants is in the late winter to early spring, when the native plants have not quite bloomed yet. You will notice a lot of green invasive plants blooming; <strong>the best practice for ridding your space of invasive plants is pulling or digging it out by the roots.</strong> This way, it is more difficult for them to grow back.</p>
             </ul>
 
         </div>
