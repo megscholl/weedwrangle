486c22e338a3229f8eafef343b1a401521eff3fb
Meg Scholl <meghan.e.scholl@gmail.com>
2018-09-30 18:41:08 -0500

    florida event location added

--- src/pages/states/Florida.js
+++ src/pages/states/Florida.js
@@ -39,8 +39,9 @@ const Florida = () =>{
                   </Card>
 
                   <div className="muscle-shoals">
-                  <h4 className="fl-h4">Jacksonville</h4>
-                      
+                  <h4 className="fl-h4"> Walter Jones Historical Park</h4>
+                  <p>Major William Webb purchased 31.2 acres on the St. Johns River in Mandarin in 1875. He built a home, barn, and 1,000-foot dock extending into the St. Johns River. Major Webb cultivated oranges,  potatoes, tomatoes, cucumbers, strawberries and beans. The farm produce was shipped north on steamships that regularly stopped in Mandarin. Walter Jones, proprietor of the Mandarin Store and Post Office, moved his family to the homestead in the early 1900s and his family members occupied the property until 1992. The City of Jacksonville acquired 10 acres of Major Webb’s original homestead from the descendants of Walter Jones in 1994 for the purpose of creating the city’s first historical park.</p>
+                  <p>The Mandarin Museum &amp; Historical Society operates the park facilities under a contract with the city. The park opened to the public in August 2000 and is today home to the Mandarin Museum, the St. Joseph’s Mission Schoolhouse for African-American Children, and other historical structures, including the 1875 Webb Farmhouse, the 1876 barn, the Losco Winery, and the Wheeler Sawmill. Following restoration, the Webb Farmhouse opened for public tours in August 2002. The Mandarin Museum &amp; Historical Society has furnished the house to reflect family life during the late 1890s and early 1900s. Mandarin now is a small section of the City of Jacksonville, Florida, but its natural beauty, parks and historic buildings draw visitors from around the world.</p>
                       
                   </div>
                 
