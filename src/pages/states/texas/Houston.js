3e5b9421ad8ebd273968e6bd2c46b219400220ee
Meg Scholl <meghan.e.scholl@gmail.com>
2018-10-01 10:36:42 -0500

    Dallas finished

---
+++ src/pages/states/texas/Houston.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+//PHOTOGRAPHY
+import HoustonLogo from './houston-logo.PNG'
+
+
+import './StateLocations.css'
+
+
+class Houston extends Component {
+    render() {
+
+        return (
+            <div>
+                    <div className="ww-logo"><img src={HoustonLogo} alt="Houston Weed Wrangle Logo" width="60%"/></div>
+
+                    
+                    
+                    <div className="lm-main-content">
+
+                    </div>
+
+                    
+                    <div className="images">
+
+                    </div>
+                    
+
+
+                    <h4 className="lm-h4">Organizational Partners:</h4>
+                    <div className="lm-gallery">
+
+
+                    </div>
+
+            </div>
+        )
+    
+    }
+}
+
+export default Houston;
